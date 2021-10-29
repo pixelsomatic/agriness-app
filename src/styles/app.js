b9d7bb0f062977f79ec18deea7ebae904554d7e6
Krsnananda <krsnananda@visie.com.br>
2021-10-29 07:56:24 -0300

    Implementa logout

---
+++ src/styles/app.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const LogoutButtom = styled.TouchableOpacity`
+  margin-right: 20px;
+  border: 1px;
+  border-color: #000;
+  border-radius: 20px;
+  padding: 2px 10px;
+`
+export const LogoutText = styled.Text`
+  font-weight: bold;
+`
+export const LogoutArea = styled.View`
+  margin-top: 20px;
+  align-items: flex-end;
+  width: 100%;
+`
