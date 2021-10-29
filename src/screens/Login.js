036f547e5be1255bdb9942c30f51683edcec6512
Krsnananda <krsnananda@visie.com.br>
2021-10-28 22:59:37 -0300

    Corrige problema com api em localhost

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Container, Fields, Input, Label, EnterArea, RegularButton, ButtonText } from "../styles/common";
+import { urls } from "../services/urls";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+
+  function Login() {
+    const options = {
+      method: 'POST',
+      url: urls.login,
+      headers: { 'Content-Type': 'application/json' },
+      data: { email: email, password: pass }
+    };
+
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
+
   return (
     <Container>
       <Fields>
         <Label>E-mail</Label>
-        <Input placeholder={'Seu e-mail'} autoCapitalize={'none'} keyboardType={'email-address'} />
+        <Input
+          placeholder={'Seu e-mail'}
+          autoCapitalize={'none'}
+          keyboardType={'email-address'}
+          onChangeText={(text) => setEmail(text)}
+        />
       </Fields>
       <Fields>
         <Label>Senha</Label>
-        <Input placeholder={'Sua senha'} autoCapitalize={'none'} secureTextEntry={true} keyboardType={'default'} />
+        <Input
+          placeholder={'Sua senha'}
+          autoCapitalize={'none'}
+          secureTextEntry={true}
+          keyboardType={'default'}
+          onChangeText={(text) => setPass(text)}
+        />
       </Fields>
       <EnterArea>
-        <RegularButton>
+        <RegularButton onPress={() => Login()} >
           <ButtonText>Entrar</ButtonText>
         </RegularButton>
       </EnterArea>
--- src/services/urls.js
+++ src/services/urls.js
@@ -1,6 +1,7 @@
-const baseUrl = ''
+const baseUrl = 'https://6768-45-224-75-181.ngrok.io'
 
 export const urls = {
+  login: baseUrl + 'api/login',
   siteUrl: '',
   checkin: baseUrl + '',
   places: baseUrl + '',
